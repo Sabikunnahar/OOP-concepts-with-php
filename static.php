3f1cd40816a210733c5977be6cb471d06adecc3f
Sabikun Nahar Bristy <30294762+Sabikunnahar@users.noreply.github.com>
2025-02-19 15:51:55 +0600

    Core: OOP static variables and functions
    Core: OOP static variables and functions

---
+++ static.php
@@ -0,0 +1,36 @@
+<?php
+/*
+    :Static variable didn't need to create an object to call the variable.
+    :Static variable is shared among all the objects of the class.  
+    :Static function is called before the constructor function.
+    :Static function can be called without creating an object.
+*/
+
+Class class1{
+    static public $num=10;
+    static function test(){
+        echo self::$num++;
+        echo "<br>";
+        echo "Static function is calling";
+
+    }
+
+}
+
+echo class1::$num;
+echo "<br>";
+echo class1::test();
+
+echo class1::test();
+echo class1::test();
+echo class1::test();
+echo class1::test();
+echo class1::test();
+
+
+
+
+
+
+
+
