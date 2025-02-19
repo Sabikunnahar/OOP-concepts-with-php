5a69b8126ad0a7696136767b7140c84cdf49c3eb
Sabikun Nahar Bristy <30294762+Sabikunnahar@users.noreply.github.com>
2025-02-19 15:17:27 +0600

    Core: OOP Interface
    Core: OOP Interface

---
+++ oop_interface.php
@@ -0,0 +1,37 @@
+<?php
+/*
+    :Suppose I have to inherit 2 classes. I cannot inherit directly from 2 classes. So, that's why we use interface.
+    : Support multiple inheritance
+    : Interface class can contain only abstract methods.
+    : In interface we cannot define variables.
+    : We can define variables in abstract class.
+    : No constructor in interface.
+    : All abstract function have to be public in interface.
+*/
+
+
+interface Interface1 {
+    public function test1();
+}
+
+interface Interface2{
+    public function test2();
+}
+
+class Interface3 implements Interface1, Interface2{ 
+    public function test1() {
+        echo "Test1";
+    }
+
+    public function test2() {
+        echo "Test2";
+    }
+}
+
+$obj_interface3 =new Interface3();
+$obj_interface3->test1();
+$obj_interface3->test2();
+
+
+
+?>
